6f6e0474ec120481ba526dc5d8a6cf6be8ca28c7
Frank Xin <frankxin@macbook-air.myfiosgateway.com>
2022-01-01 00:23:34 -0500

    Error text fix

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -43,7 +43,7 @@ export const Product = () => {
     return (
       <Link to="/">
         <h1 className="text-3xl dark:text-neutral-50 text-gray-800 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          This link expired, click to return home
+          This link has expired, click to return home
         </h1>
       </Link>
     );
